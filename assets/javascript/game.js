06297d3a551f194755449f331a19e123ebb5d874
Andrew Lee <andrewimg@gmail.com>
2020-03-07 08:09:05 -0800

    added ID selectors to crystals and score values in html. Added javascript and Jquery for full functionality of the game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,59 @@
-$("#crystal").on("click", function(){
-console.log("something")
-})
-$("#crystal2").on("click", function(){
-    console.log("something2")
-    })
-$("#crystal3").on("click", function(){
-        console.log("something3")
-        })
-$("#crystal4").on("click", function(){
-            console.log("something4")
-            })
+var playerScore = 0;
+var randomNumber = 0;
+var wins = 0;
+var losses = 0;
+var crystalValue = [];
+
+
+//19-120
+randomNumber = Math.floor(Math.random() * 101) + 19
+
+$("#random").text(randomNumber);
+$("#wins").text(wins);
+$("#losses").text(losses);
+$("#sum").text(playerScore)
+
+crystalValue.push(Math.floor(Math.random() * 11) + 1);
+crystalValue.push(Math.floor(Math.random() * 11) + 1);
+crystalValue.push(Math.floor(Math.random() * 11) + 1);
+crystalValue.push(Math.floor(Math.random() * 11) + 1);
+
+$(".crystal").on("click", function () {
+   // console.log("something")
+    var getIndex = $(this).attr("data-index");
+   // console.log(crystalValue[getIndex])
+    playerScore += crystalValue[getIndex];
+   // console.log(playerScore)
+    $("#sum").text(playerScore);
+
+    if (playerScore === randomNumber) {
+        wins++;
+        $("#wins").html(wins);
+        reset();
+
+    }
+    if (playerScore > randomNumber) {
+        losses++;
+        $("#losses").html(losses);
+        reset();
+    }
+
+
+});
+
+
+function reset() {
+    crystalValue = []
+    crystalValue.push(Math.floor(Math.random() * 11) + 1)
+    crystalValue.push(Math.floor(Math.random() * 11) + 1)
+    crystalValue.push(Math.floor(Math.random() * 11) + 1)
+    crystalValue.push(Math.floor(Math.random() * 11) + 1)
+    randomNumber = Math.floor(Math.random() * 101) + 19
+
+    playerScore = 0
+    $("#sum").text(playerScore);
+    $("#random").text(randomNumber);
+
+};
+
+
