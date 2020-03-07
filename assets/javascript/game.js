08bd3c4a062c8abcede9e37c83a7f90b21de96c6
Andrew Lee <andrewimg@gmail.com>
2020-03-06 19:25:33 -0800

    added new id's to crystal images. Added jquery functions with console.logs for when the images are clicked.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,12 @@
 $("#crystal").on("click", function(){
-console.log(something)
+console.log("something")
 })
+$("#crystal2").on("click", function(){
+    console.log("something2")
+    })
+$("#crystal3").on("click", function(){
+        console.log("something3")
+        })
+$("#crystal4").on("click", function(){
+            console.log("something4")
+            })
